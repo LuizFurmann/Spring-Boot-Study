6fabf3d5f726285dba3cae1517b4c932fda34ec5
Luiz Furmann <furmannluiz@gmail.com>
2024-02-13 22:51:37 -0300

    criando classe de model

--- src/main/java/com/example/study/models/Task.java
+++ src/main/java/com/example/study/models/Task.java
@@ -1,6 +1,10 @@
 package com.example.study.models;
 
+import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 @Table(name = Task.TABLE_TASK)
@@ -13,8 +17,102 @@ public class Task {
     private Integer id;
 
     @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
     @Column(name = "description", length = 255, nullable = false)
+    @NonNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
     private String desciption;
+
+
+    public Task() {
+    }
+
+    public Task(Integer id, User user, String desciption) {
+        this.id = id;
+        this.user = user;
+        this.desciption = desciption;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDesciption() {
+        return this.desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    public Task id(Integer id) {
+        setId(id);
+        return this;
+    }
+
+    public Task user(User user) {
+        setUser(user);
+        return this;
+    }
+
+    public Task desciption(String desciption) {
+        setDesciption(desciption);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this)
+            return true;
+        if(!(obj instanceof User)){
+            return false;
+        }
+        if(obj == null){
+            return false;
+        }
+
+        Task task = (Task) obj;
+
+        if(this.id == null)
+            if(task.id != null)
+                return false;
+            else {
+                return false;
+        }
+
+        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(desciption, task.desciption);
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (this.id == null ? 0 : this.id.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", user='" + getUser() + "'" +
+            ", desciption='" + getDesciption() + "'" +
+            "}";
+    }
+    
 }
